b40987d406757f52e5b616fc5e854dad498725b3
David Simon <me@davidsimon.ch>
2014-04-06 00:15:41 +0200

    [Update] Header background fade

--- 03_Studierende/David_Simon/07_Tatort_New/html/js/main.js
+++ 03_Studierende/David_Simon/07_Tatort_New/html/js/main.js
@@ -11,8 +11,18 @@ $(document).ready(function () {
     }
 
     // Random Header Background
-    var availableBackgrounds = 2,
-        randBackgroundInt = Math.round(1+Math.random()*(availableBackgrounds-1));
-    $('.layout__header-container').addClass('background-' + randBackgroundInt);
-
+    var shuffleArray = function (v) {
+            /* jshint ignore:start */
+            for(var j, x, i = v.length; i; j = parseInt(Math.random() * i), x = v[--i], v[i] = v[j], v[j] = x);
+            /* jshint ignore:end */
+            return v;
+        }
+      , availableBackgrounds = shuffleArray(['schimanski', 'kurtulus', 'bienzle'])
+      , currentBackground = 0;
+    setInterval(function () {
+        var newBackground = (currentBackground < availableBackgrounds.length) ? currentBackground+1 : 0;
+        console.log(newBackground);
+        $('.layout__header-container').removeClass('background-' + availableBackgrounds[currentBackground]).addClass('background-' + availableBackgrounds[newBackground]);
+        currentBackground = newBackground;
+    },5000);
 });
